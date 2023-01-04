73cde0419eca1df6ac5c3a3627515acd3099336a
NguyenHuongGiang128 <studenthumg2000@gmail.com>
2023-01-04 08:02:44 +0700

    First_commit_NHG800

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NguyenHuongGiang800.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
